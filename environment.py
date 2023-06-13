4987d30f19c096b7b9c30cfa95d340b5c4a95dc6
Dio <a0981551262@gmail.com>
2023-06-13 15:32:56 +0800

    1. environment programming

---
+++ environment.py
@@ -0,0 +1,22 @@
+import random
+
+
+def play_baccarat(t, amount):
+    odds = {'banker': 1.95, 'player': 2, 'tie': 9, 'pair': 12}
+    playing_result = random.random()
+
+    if playing_result < 0.458597 and t == 'banker':
+        return amount * odds[t]
+    elif 0.458597 <= playing_result < 0.904844 and t == 'player':
+        return amount * odds[t]
+    elif 0.904844 <= playing_result < 1 and t == 'tie':
+        return amount * odds[t]
+    elif t == 'pair' and playing_result < 0.074683:
+        return amount * odds[t]
+
+    return 0
+
+
+if __name__ == '__main__':
+    for _ in range(100):
+        print(play_baccarat('banker', 10000))
